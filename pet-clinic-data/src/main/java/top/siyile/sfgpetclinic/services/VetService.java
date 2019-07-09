13f3024b36946f1cbf0675178035844b7a2f1e33
siyi.le <seayahh@outlook.com>
2019-07-09 11:08:03 +0800

    refactoring to common crud interface. Closes #11.

--- pet-clinic-data/src/main/java/top/siyile/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/top/siyile/sfgpetclinic/services/VetService.java
@@ -2,13 +2,6 @@ package top.siyile.sfgpetclinic.services;
 
 import top.siyile.sfgpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
