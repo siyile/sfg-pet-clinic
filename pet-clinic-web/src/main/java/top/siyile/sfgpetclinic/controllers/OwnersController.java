1e575d44aadb7981f110d0cfb6d307359a2a9ad4
siyi.le <seayahh@outlook.com>
2019-07-09 18:07:11 +0800

    added Owners List and updated controller #16.

--- pet-clinic-web/src/main/java/top/siyile/sfgpetclinic/controllers/OwnersController.java
+++ pet-clinic-web/src/main/java/top/siyile/sfgpetclinic/controllers/OwnersController.java
@@ -1,14 +1,24 @@
 package top.siyile.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import top.siyile.sfgpetclinic.services.OwnerService;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnersController {
 
+    private final OwnerService ownerService;
+
+    public OwnersController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+
+        model.addAttribute("owners", ownerService.findAll());
 
         return "owners/index";
     }
