16f535c6d3916a63afc0b61548cad4c2b0daa50a
siyi.le <seayahh@outlook.com>
2019-07-09 11:24:35 +0800

    Impl Map based Service. Closes #6.

---
+++ pet-clinic-data/src/main/java/top/siyile/sfgpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,35 @@
+package top.siyile.sfgpetclinic.services.map;
+
+import top.siyile.sfgpetclinic.model.Pet;
+import top.siyile.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+}
