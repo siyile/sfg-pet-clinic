e72b339bb465dc201eaa927816af9a36bd6f77ed
siyi.le <seayahh@outlook.com>
2019-07-09 18:11:11 +0800

    added Vets List and updated controller #15.

--- pet-clinic-web/src/main/java/top/siyile/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/top/siyile/sfgpetclinic/controllers/VetController.java
@@ -1,13 +1,23 @@
 package top.siyile.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import top.siyile.sfgpetclinic.services.VetService;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+
+        model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
     }
