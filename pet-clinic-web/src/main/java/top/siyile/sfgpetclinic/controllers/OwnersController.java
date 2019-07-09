486051e1c9d4e8770f68e59bb753e6533acc74f5
siyi.le <seayahh@outlook.com>
2019-07-09 12:07:50 +0800

    Adding owners page and Controller. Closes #13.

---
+++ pet-clinic-web/src/main/java/top/siyile/sfgpetclinic/controllers/OwnersController.java
@@ -0,0 +1,15 @@
+package top.siyile.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnersController {
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listVets() {
+
+        return "owners/index";
+    }
+}
